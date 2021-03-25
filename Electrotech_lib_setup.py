f6a8529fd2129f0e7168edccb7e359e35af52ef4
Stanislav K <delphi.troll@mail.ru>
2021-03-25 17:07:13 +0500

    Adding libs download in setup file

--- Electrotech_lib_setup.py
+++ Electrotech_lib_setup.py
@@ -1,18 +1,26 @@
 import subprocess
 import sys
-import requests
-import zipfile
 
 
 def main():
+    try:
+        import requests
+    except:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+    try:
+        import zipfile
+    except:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "zipfile"])
+
     subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQt5"])
 
-    url = ""
+    url = "https://github.com/qerty123/electrotech_lib/archive/refs/heads/main.zip"
     r = requests.get(url)
     filename = url.split("/")[-1]
+    extractpath = "."
     with zipfile.ZipFile(filename, 'r') as zip:
-        zip.extractall()
+        zip.extractall(path=extractpath)
 
 
 if __name__ == "__main__":
